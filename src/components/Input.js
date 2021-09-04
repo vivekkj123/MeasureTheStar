ec82502240773fe63ad1a7cc456db7b8bed153b7
Vivek K J <vivekkj2004@gmail.com>
2021-09-04 19:39:36 +0530

    Configure input

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import './Input.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import "./Input.css";
+import { Octokit } from "@octokit/core";
+import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
+const octokit = new Octokit({ auth: process.env.AUTH_API});
 
 function Input() {
+    const [StarCount, setStarCount] = useState(0);
+    const [Text, setText] = useState("");
+    let getStarcount = async(Text) => {
+        let spilitArr = Text.split('/')
+        let username = spilitArr[0]
+        let repo_name = spilitArr[1]
+        const response = await octokit.request(`GET /repos/${username}/${repo_name}`, {
+          }).then((response)=>{
+              console.log(response.data.stargazers_count)
+              setStarCount(response.data.stargazers_count)
+          }).catch((err)=>{
+              console.log(err)
+              console.error(err.message)
+          })	
+    };
     return (
-        <div className="Input">
-            <input placeholder="Github Repo URL" className='Input__box' type="text" />
-            <input type="button" value="Submit" />
-        </div>
-    )
+    <div className="Input">
+      <input
+        placeholder="username/repo"
+        className="Input__box"
+        type="text"
+        value={Text}
+        onChange={(e)=> setText(e.target.value)}
+      />
+      <button onClick={()=>{getStarcount(Text)}}>
+        <FontAwesomeIcon icon={faArrowAltCircleRight} />
+      </button>
+    </div>
+  );
 }
 
-export default Input
+export default Input;
