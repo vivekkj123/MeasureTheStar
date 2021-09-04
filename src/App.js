6e582ccdc2f99b72214b2eb1302330a655e5b83f
Vivek K J <vivekkj2004@gmail.com>
2021-09-04 21:51:37 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import MainSection from './components/MainSection'
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className='App'>
       <Header/>
       <MainSection/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className="Footer">
+            <h3>A Tinkerhub Saturday Hack Night Product</h3>
+            <h4>Made by Vivek K J with ♥</h4>
+        </div>
+    )
+}
+
+export default Footer
