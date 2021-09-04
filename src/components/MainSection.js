898eac256163c7e4909e054f2abd167fe04a307b
Vivek K J <vivekkj2004@gmail.com>
2021-09-04 21:36:16 +0530

    Add some confetti 🎉

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -2,49 +2,67 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import "./MainSection.css";
 import { Octokit } from "@octokit/core";
-import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
-import Star from './Star'
-const octokit = new Octokit({ auth: process.env.AUTH_API});
+import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons";
+import Star from "./Star";
+import Confetti from "react-confetti";
+
+const octokit = new Octokit({ auth: process.env.AUTH_API });
 
 function MainSection() {
-    const [StarCount, setStarCount] = useState(0);
-    const [RenderStar, setRenderStar] = useState(false)
-    const [Text, setText] = useState("");
-    let getStarcount = async(Text) => {
-        let spilitArr = Text.split('/')
-        let username = spilitArr[0]
-        let repo_name = spilitArr[1]
-        // eslint-disable-next-line
-        const response = await octokit.request(`GET /repos/${username}/${repo_name}`, {
-          }).then((response)=>{
-              console.log(response.data.stargazers_count)
-              setStarCount(response.data.stargazers_count)
-          }).catch((err)=>{
-              console.log(err)
-              console.error(err.message)
-          })	
-          setRenderStar(true)
-
-    };
-    return (<div className="mainSection">
-    <div className="Input">
-      <input
-        placeholder="username/repo"
-        className="Input__box"
-        type="text"
-        value={Text}
-        onChange={(e)=> setText(e.target.value)}
-      />
-      <button className='input__btn' onClick={()=>{getStarcount(Text)}}>
-        <FontAwesomeIcon icon={faArrowAltCircleRight}/>
-      </button>
+  const [StarCount, setStarCount] = useState(0);
+  const [RenderStar, setRenderStar] = useState(false);
+  const [Text, setText] = useState("");
+  let getStarcount = async (Text) => {
+    let spilitArr = Text.split("/");
+    let username = spilitArr[0];
+    let repo_name = spilitArr[1];
+    // eslint-disable-next-line
+    const response = await octokit
+      .request(`GET /repos/${username}/${repo_name}`, {})
+      .then((response) => {
+        console.log(response.data.stargazers_count);
+        setStarCount(response.data.stargazers_count);
+      })
+      .catch((err) => {
+        console.log(err);
+        console.error(err.message);
+      });
+    setRenderStar(true);
+  };
+  return (
+    <div className="mainSection">
+      <div className="Input">
+        <input
+          placeholder="username/repo"
+          className="Input__box"
+          type="text"
+          value={Text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button
+          className="input__btn"
+          onClick={() => {
+            getStarcount(Text);
+          }}
+        >
+          <FontAwesomeIcon icon={faArrowAltCircleRight} />
+        </button>
+      </div>
+      <div>
+        {" "}
+        {RenderStar ? (
+          <>
+            <Star size={StarCount} />
+            <Confetti
+              width={window.innerWidth}
+              height={window.innerHeight}
+              numberOfPieces={2000}
+              recycle={false}
+            />
+          </>
+        ) : null}{" "}
+      </div>
     </div>
-    <div>
-      
-      {RenderStar?<Star size={StarCount}/>:null}
-    </div>
-    </div>
-    
   );
 }
 
--- src/components/Star.js
+++ src/components/Star.js
@@ -2,14 +2,14 @@ import React from 'react'
 import './Star.css'
 function Star(props) {
     let size = props.size
-    let fontSize = size>500?500:size;
+    let fontSize = size>1000?1000:size;
     let text = ''
     if(size===0){
         text = "Gain some stars before hitting me 🙅🏻‍♂"
+    }else if(size>1000){
+        text = "You made me happy.. You are a MONSTAR!!!!!!"
     }else if(0<size<1000){
         text = `Wow you have ${size} stars, make it a monstar !!!`
-    }else{
-        text = "You made me happy.. You are a MONSTAR!!!!!!"
     }
     return (
         <div>
